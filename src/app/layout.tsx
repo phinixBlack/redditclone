230d49853150985f42434cd5df4f73ddd4ed33cb
phenixDigibeee <139323533+phenixDigibeee@users.noreply.github.com>
2023-12-08 17:10:38 +0530

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
+import { cn } from '@/lib/utils';
 import '@/styles/globals.css'
-
+import { Inter } from "next/font/google"
+import Navbar from '@/components/Navbar';
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
-
+const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en' className={cn('bg-white text-slate-900 antialiased ligh', inter.className)}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialised'>
+      <Navbar />
+        <div className='cantainer max-w-7xl mx-auto h-full pt-12'>
+           {children}
+          </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import { Icons } from "./Icons"
+import { buttonVariants } from "./ui/Button"
+
+
+const Navbar = () => {
+   return <div className="fixed top-0 inset-x-0 h-fit bg-zinc 100 border-b border-zinc-300 z-[10] py-2" ><div className="container mxa-w-7xl h-full mx-auto flex intems-center justify-betwwen gap-2">
+    {/*logo*/}
+    <Link href="/" className="flex gap-2 itmes-center" >
+        <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6"></Icons.logo>
+        <p className="hidden text-zinc-700 text-sm font-medium md:block">NikhilProject</p>
+    </Link>
+      {/*search bar*/}
+      <Link href="/sign-in" className={buttonVariants()}>Sign In</Link>
+    </div></div>
+}
+export default Navbar
